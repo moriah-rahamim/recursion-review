62632ab2add9773d563c4cf4a05cf6844f38da69
moriah-rahamim <moriahfaye@gmail.com>
2018-01-15 16:15:18 -0800

    implemented getElementsByClassName.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,35 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+
+  var getChildrenByClassName = function(element) {
+
+    let elementsWithClassName = [];
+
+    // no matter what:
+    //   check if class name exists
+    //   if yes, add to result
+    //   if no, start result empty
+    if (element.classList) {
+      if (element.classList.contains(className)) {
+        elementsWithClassName.push(element);
+      }
+    }
+    // debugger;
+    // recursive case:
+    //   element has children
+    //   return result plus value of recursive calls on each child
+    if (element.hasChildNodes()) {
+      let children = element.childNodes;
+      for (let i = 0; i < children.length; i++) {
+        elementsWithClassName = elementsWithClassName.concat(getChildrenByClassName(children[i]));
+      }
+    }
+
+    // in base case of no children, this still returns correct elements
+    return elementsWithClassName;
+  };
+
+  return getChildrenByClassName(document.body);
 };
